cc07fc91d7b43e2d641e9af03e648edb9e90bcf6
richard <richard@bka.co.nz>
2017-02-09 13:52:56 +1300

    update

--- sync.bat
+++ sync.bat
@@ -1,9 +1,4 @@
 @echo off
-ECHO SOURCE BRANCH IS %BUILD_SOURCEBRANCH%
-IF %BUILD_SOURCEBRANCH% == refs/heads/master (
-   ECHO Building master branch so no merge is needed.
-   EXIT
-)
 SET sourceBranch=origin/%BUILD_SOURCEBRANCH:refs/heads/=%
 ECHO GIT CHECKOUT MASTER
 git checkout master
